430fd3f00ebec415e2ebfedb73c1c310ee2e6467
mingfan.wei <mingfan.wei@gmail.com>
2017-05-31 18:51:51 +0800

    2017053101(Interpolation and peaks)
    ///////////////////////
    1. Interpolation to 500Hz.
    2. Filter by FIR 16-steps.
    3. Find peaks by so & chan.
    
    Results:
    
    1. Interpolation OK.
    2. Filter OK, but can not use butterworth 2-nd order filter.
    3. Can find peaks by so-n-chan method, but not correspon with papers.

--- PVCs.m
+++ PVCs.m
@@ -85,33 +85,43 @@ for i = 1:size(val, 1)
 end
 
 x = (1:size(val, 2)) * interval;
-
 x=x';
 val=val';
 % for j = 1:size(x, 1)-2
 %     val1(j) = ((val(j+2,2) -val(j+1,2)) / (x(j+2) - x(j+1)))* (x(j) - x(j+1)) + val(j+1,2);
 % end
-
-
-% val1=val1';
-% val1=x;
+val1 = interp(val(:,1),4);
+val3 = interp(val(:,3),4);
+x1 = interp(x,4);
+% https://cn.mathworks.com/help/signal/ref/interp.html
+% t = 0:0.001:1;
+% x = sin(2*pi*30*t) + sin(2*pi*60*t);
+% y = interp(x,4);
 % 
-% for j = 1:1:1000
-%      val1(j) = (val(j, 3) + val(j+1, 3))/3;
-% end
+% subplot 211
+% stem(0:30,x(1:31),'filled','markersize',3)
+% grid on
+% xlabel 'Sample number',ylabel Original
+% subplot 212
+% stem(0:120,y(1:121),'filled','markersize',3)
+% grid on
+% xlabel 'Sample number',ylabel Interpolated
 
-range = 750000;
-
+% range = 750000;  % 750000 samples = 750000x0.008(1/125)seconds = 6000 seconds
+range = 1000; 
 plot(x(1:range), val(1:range,1));
 hold on
 plot(x(1:range), val(1:range,3),'r');
 hold on
-% plot(x(1:range), val1(1:range),'c');
+plot(x1(1:4*range), val1(1:4*range),'cx');
+plot(x1(1:4*range), val3(1:4*range),'y+');
 hold on
 for i = 1:length(signal)
     labels{i} = strcat(signal{i}, ' (', units{i}, ')'); 
 end
-legend(labels{1,1},labels{1,3});
+labels{1,8} = 'Interpo ECG';
+labels{1,9} = 'Interpo PPG';
+legend(labels{1,1},labels{1,3},labels{1,8},labels{1,9} );
 
 xlabel('Time (sec)');
 grid on;
@@ -119,18 +129,72 @@ grid on;
 hold off
 % figure(2)
 % plot(x(1:1000), val1(1:1000),'c');
+%% Preprocessing
+figure(1)
+range = 750000*4; 
+val16000s = val1(1:range);
+val36000s = val3(1:range);
+x16000s = x1(1:range);
+plot(x16000s(1:1000),val16000s(1:1000));
+hold on;
+plot(x16000s(1:1000),val36000s(1:1000),'r');
+grid on;
+xlabel('Time (sec)');
+% hold off;
+% filter 
 
 
-%%
-% x = 0:1:4*pi;  
-% y = sin(x).*exp(-x/5);  
-% xi = 0:0.1:4*pi;  
-% y1 = interp1(x, y, xi, 'nearest');  
-% y2 = interp1(x, y, xi, 'linear');  
-% y3 = interp1(x, y, xi, 'pchip');  
-% y4 = interp1(x, y, xi, 'spline');  
-% plot(x, y, 'o', xi, y1, xi, y2, xi, y3, xi, y4);  
-% legend('Original', 'Nearest', 'Linear', 'Pchip', 'Spline');
+figure(2)
+
+% Method 1:
+
+% fs=500;		% Sampling rate
+% filterOrder=2;		% Order of filter
+% cutOffFreq1=0.001;	% Cutoff frequency
+% cutOffFreq2=0.007;	% Cutoff frequency
+% [b, a]=butter(filterOrder, [cutOffFreq1 cutOffFreq2], 'bandpass');
+% % === Plot frequency response
+% [h, w]=freqz(b, a);
+% plot(w/pi*fs/2, abs(h), '.-'); title('Magnitude frequency response');
+% grid on
+
+% Method 2:
+b = fir1(16,[0.00055 0.22223]);
+freqz(b)
+
+figure(3)
+B= val16000s; 
+val16000sf = filter(b,1,B);
+Bp= val36000s; 
+val36000sf = filter(b,1,Bp);
+plot(x16000s(1:3000),val16000s(1:3000));
+hold on;
+plot(x16000s(1:3000),val36000s(1:3000),'r');
+hold on;
+plot(x16000s(1:3000),val16000sf(1:3000),'c');
+hold on;
+plot(x16000s(1:3000),val36000sf(1:3000),'y');
+grid on;
+xlabel('Time (sec)');
+ylabel('Amp (mv)');
+
+%% find beats
+
+% Method 1:
+Fs=500;
+ so_chan(val16000sf(1:1000000),Fs) 
+
+so_chan(val16000sf(1000001:2000000),Fs) 
+
+so_chan(val16000sf(2000001:3000000),Fs) 
+
+% Method 2:
+
+[R, Rt] = findpeaks(val16000sf);                 % Find R-Waves & Times
+% EKGf=(fix(val16000sf));
+% [R, Rt] = findpeaks(EKGf, Fs, 'MinPeakHeight',500);
+figure(3)                                                           % Plot Filtered EKG
+plot(x16000s(1:3000),val16000sf(1:3000),'y');
 
 % grid on
 % % display('***This example will write a  Ex1.dat and Ex1.hea file to your current directory!')
